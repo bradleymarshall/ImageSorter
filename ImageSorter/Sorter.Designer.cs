c0f67c11ea7a860162915a2f21d43ad8ba58cba2
Bradley Marshall <bradandrenee@gmail.com>
2017-01-18 22:49:07 -0500

    Initial commit of ImageSorter project.

---
+++ ImageSorter/Sorter.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace ImageSorter
+{
+    partial class FormImageSorter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.textSourcePath = new System.Windows.Forms.TextBox();
+            this.labelSource = new System.Windows.Forms.Label();
+            this.labelDest = new System.Windows.Forms.Label();
+            this.textDestPath = new System.Windows.Forms.TextBox();
+            this.buttonSource = new System.Windows.Forms.Button();
+            this.buttonDest = new System.Windows.Forms.Button();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.browserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonSortVids = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(17, 85);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 0;
+            this.buttonTest.Text = "Test Images";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(572, 85);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Close";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // textSourcePath
+            // 
+            this.textSourcePath.Location = new System.Drawing.Point(83, 12);
+            this.textSourcePath.Name = "textSourcePath";
+            this.textSourcePath.Size = new System.Drawing.Size(483, 20);
+            this.textSourcePath.TabIndex = 2;
+            // 
+            // labelSource
+            // 
+            this.labelSource.AutoSize = true;
+            this.labelSource.Location = new System.Drawing.Point(33, 15);
+            this.labelSource.Name = "labelSource";
+            this.labelSource.Size = new System.Drawing.Size(44, 13);
+            this.labelSource.TabIndex = 3;
+            this.labelSource.Text = "Source:";
+            this.labelSource.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // labelDest
+            // 
+            this.labelDest.AutoSize = true;
+            this.labelDest.Location = new System.Drawing.Point(14, 41);
+            this.labelDest.Name = "labelDest";
+            this.labelDest.Size = new System.Drawing.Size(63, 13);
+            this.labelDest.TabIndex = 5;
+            this.labelDest.Text = "Destination:";
+            this.labelDest.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // textDestPath
+            // 
+            this.textDestPath.Location = new System.Drawing.Point(83, 38);
+            this.textDestPath.Name = "textDestPath";
+            this.textDestPath.Size = new System.Drawing.Size(483, 20);
+            this.textDestPath.TabIndex = 4;
+            // 
+            // buttonSource
+            // 
+            this.buttonSource.Location = new System.Drawing.Point(572, 10);
+            this.buttonSource.Name = "buttonSource";
+            this.buttonSource.Size = new System.Drawing.Size(75, 23);
+            this.buttonSource.TabIndex = 6;
+            this.buttonSource.Text = "Browse...";
+            this.buttonSource.UseVisualStyleBackColor = true;
+            this.buttonSource.Click += new System.EventHandler(this.buttonSource_Click);
+            // 
+            // buttonDest
+            // 
+            this.buttonDest.Location = new System.Drawing.Point(572, 37);
+            this.buttonDest.Name = "buttonDest";
+            this.buttonDest.Size = new System.Drawing.Size(75, 23);
+            this.buttonDest.TabIndex = 7;
+            this.buttonDest.Text = "Browse...";
+            this.buttonDest.UseVisualStyleBackColor = true;
+            this.buttonDest.Click += new System.EventHandler(this.buttonDest_Click);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(410, 85);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(75, 23);
+            this.buttonSort.TabIndex = 8;
+            this.buttonSort.Text = "Sort Images";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.buttonSort_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(98, 85);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(306, 23);
+            this.progressBar.TabIndex = 9;
+            this.progressBar.Visible = false;
+            // 
+            // browserDialog
+            // 
+            this.browserDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(215, 69);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(47, 13);
+            this.labelStatus.TabIndex = 10;
+            this.labelStatus.Text = "<status>";
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // buttonSortVids
+            // 
+            this.buttonSortVids.Location = new System.Drawing.Point(491, 85);
+            this.buttonSortVids.Name = "buttonSortVids";
+            this.buttonSortVids.Size = new System.Drawing.Size(75, 23);
+            this.buttonSortVids.TabIndex = 11;
+            this.buttonSortVids.Text = "Sort Videos";
+            this.buttonSortVids.UseVisualStyleBackColor = true;
+            this.buttonSortVids.Click += new System.EventHandler(this.buttonSortVids_Click);
+            // 
+            // FormImageSorter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 126);
+            this.Controls.Add(this.buttonSortVids);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.buttonDest);
+            this.Controls.Add(this.buttonSource);
+            this.Controls.Add(this.labelDest);
+            this.Controls.Add(this.textDestPath);
+            this.Controls.Add(this.labelSource);
+            this.Controls.Add(this.textSourcePath);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonTest);
+            this.Name = "FormImageSorter";
+            this.Text = "ImageSorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.TextBox textSourcePath;
+        private System.Windows.Forms.Label labelSource;
+        private System.Windows.Forms.Label labelDest;
+        private System.Windows.Forms.TextBox textDestPath;
+        private System.Windows.Forms.Button buttonSource;
+        private System.Windows.Forms.Button buttonDest;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.FolderBrowserDialog browserDialog;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonSortVids;
+    }
+}
+
---
+++ ImageSorter/Sorter.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace ImageSorter
+{
+    public partial class FormImageSorter : Form
+    {
+        //we init this once so that if the function is repeatedly called
+        //it isn't stressing the garbage man
+        private static Regex reg = new Regex(":");
+
+        public FormImageSorter()
+        {
+            InitializeComponent();
+
+            //blank out place-holder text
+            labelStatus.Text = "";
+        }
+
+        private void buttonSource_Click(object sender, EventArgs e)
+        {
+            //query the user for a source path, where we will pull the pictures to be sorted.
+            browserDialog.Description = "Please select a source path (with the pictures to be sorted).";
+            if (browserDialog.ShowDialog(FormImageSorter.ActiveForm) == DialogResult.OK)
+                textSourcePath.Text = browserDialog.SelectedPath;
+        }
+
+        private void buttonDest_Click(object sender, EventArgs e)
+        {
+            //query the user for a destination path, where we will put the sorted pictures.
+            browserDialog.Description = "Please select a destination path (where the pictures will go).";
+            if (browserDialog.ShowDialog(FormImageSorter.ActiveForm) == DialogResult.OK)
+                textDestPath.Text = browserDialog.SelectedPath;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonTest_Click(object sender, EventArgs e)
+        {
+            //grab and validate paths
+            string sourcePath = textSourcePath.Text;
+            string destPath = textDestPath.Text;
+            if (!ValidPaths(sourcePath, destPath))
+                return;
+
+            //update status
+            labelStatus.Text = "Gathering file list...";
+            labelStatus.Refresh();
+
+            //grab the paths of all JPG files from the source directory
+            string[] filePaths = Directory.GetFiles(sourcePath, "*.jpg", SearchOption.AllDirectories);
+
+            //get the sorted list of files
+            Dictionary<string, List<string>> sorted = GetSortedFiles(filePaths, sourcePath);
+
+            //commit the list to a file (this is the test, remember?)
+            labelStatus.Text = "Writing sort to file...";
+            labelStatus.Refresh();
+            using (StreamWriter sw = new StreamWriter(Path.Combine(destPath, "SortedList.txt")))
+                foreach (var entry in sorted)
+                    sw.WriteLine(string.Format("{0}\r\n   {1}", entry.Key, string.Join("\r\n   ", entry.Value.ToArray())));
+
+            //notify the user that it's complete.
+            progressBar.Value = 100;
+            labelStatus.Text = "Complete.";
+            this.Refresh();
+        }
+
+        private void buttonSort_Click(object sender, EventArgs e)
+        {
+            //grab and validate paths
+            string sourcePath = textSourcePath.Text;
+            string destPath = textDestPath.Text;
+            if (!ValidPaths(sourcePath, destPath))
+                return;
+
+            //get the sorted list of files
+            Dictionary<string, List<string>> sorted = GetSortedFiles(
+                Directory.GetFiles(sourcePath, "*.jpg", SearchOption.AllDirectories), sourcePath);
+
+            //Let's put the sorted list into folders by date
+            progressBar.Value = 0;
+            labelStatus.Text = "Moving files...";
+            this.Refresh();
+
+            //perform the moves
+            string fileName, newPath, fullDestPath;
+            foreach (KeyValuePair<string, List<string>> imageFolder in sorted)
+            {
+                //figure and create destination folder for images
+                fullDestPath = Path.Combine(destPath, imageFolder.Key);
+                if (!Directory.Exists(fullDestPath))
+                    Directory.CreateDirectory(fullDestPath);
+
+                //move each image into it
+                foreach (string path in imageFolder.Value)
+                {
+                    //Use static Path methods to extract only the file name from the path.
+                    fileName = Path.GetFileName(path);
+                    newPath = Path.Combine(fullDestPath, fileName);
+
+                    if (File.Exists(newPath))
+                    {
+                        string fDir = Path.GetDirectoryName(newPath);
+                        string fName = Path.GetFileNameWithoutExtension(newPath);
+                        string fExt = Path.GetExtension(newPath);
+
+                        int suffix = 1;
+                        do
+                        {
+                            newPath = Path.Combine(fDir, String.Concat(fName, "-dupe" + (suffix++), fExt));
+                        } while (File.Exists(newPath));
+                    }
+                    File.Move(path, newPath);
+                }
+            }
+
+            //notify the user that it's complete.
+            progressBar.Value = 100;
+            labelStatus.Text = "Complete.";
+            this.Refresh();
+        }
+
+        private void buttonSortVids_Click(object sender, EventArgs e)
+        {
+            //grab and validate paths
+            string sourcePath = textSourcePath.Text;
+            string destPath = textDestPath.Text;
+            if (!ValidPaths(sourcePath, destPath))
+                return;
+
+            //get the sorted list of files
+            Dictionary<string, List<string>> sorted = GetSortedFiles(
+                Directory.GetFiles(sourcePath, "*.mp4", SearchOption.AllDirectories), sourcePath, true);
+
+            //Let's put the sorted list into folders by date
+            progressBar.Value = 0;
+            labelStatus.Text = "Moving files...";
+            this.Refresh();
+
+            //perform the moves
+            string fileName, newPath, fullDestPath;
+            foreach (KeyValuePair<string, List<string>> imageFolder in sorted)
+            {
+                //figure and create destination folder for images
+                fullDestPath = Path.Combine(destPath, imageFolder.Key);
+                if (!Directory.Exists(fullDestPath))
+                    Directory.CreateDirectory(fullDestPath);
+
+                //move each image into it
+                foreach (string path in imageFolder.Value)
+                {
+                    //Use static Path methods to extract only the file name from the path.
+                    fileName = Path.GetFileName(path);
+                    newPath = Path.Combine(fullDestPath, fileName);
+
+                    if (File.Exists(newPath))
+                    {
+                        string fDir = Path.GetDirectoryName(newPath);
+                        string fName = Path.GetFileNameWithoutExtension(newPath);
+                        string fExt = Path.GetExtension(newPath);
+
+                        int suffix = 1;
+                        do
+                        {
+                            newPath = Path.Combine(fDir, String.Concat(fName, "-dupe" + (suffix++), fExt));
+                        } while (File.Exists(newPath));
+                    }
+                    File.Move(path, newPath);
+                }
+            }
+
+            //notify the user that it's complete.
+            progressBar.Value = 100;
+            labelStatus.Text = "Complete.";
+            this.Refresh();
+        }
+
+        private bool ValidPaths(string source, string dest)
+        {
+            //just make sure both diretories are entered and exist
+            if (string.IsNullOrEmpty(source) || string.IsNullOrEmpty(dest) ||
+                !Directory.Exists(source) || !Directory.Exists(dest))
+            {
+                MessageBox.Show("Please enter valid source and destination paths.");
+                return false;
+            }
+            else
+                return true;
+        }
+
+        private Dictionary<string, List<string>> GetSortedFiles(string[] paths, string source, bool getVids = false)
+        {
+            progressBar.Value = 0;
+            progressBar.Visible = true;
+
+            labelStatus.Text = "Arranging files...";
+            labelStatus.Refresh();
+
+            //the string representing the Date Taken for the image, and the sorted Dictionary
+            string fileDate;
+            Dictionary<string, List<string>> sortedDict = new Dictionary<string, List<string>>();
+
+            int fileCount = paths.Length;
+            for (int i = 0; i < fileCount; i++)
+            {
+                //update progress
+                int progress = (int)Math.Round((double)i / (double)fileCount * 100);
+                if (progressBar.Value != progress)
+                {
+                    //only refresh the whole screen when we are modifying the progress bar value.
+                    //this allows the image sorting process to have more processor priority.
+                    progressBar.Value = progress;
+                    this.Refresh();
+                }
+
+                if (getVids)
+                    fileDate = GetDateModified(paths[i]).ToString("yyyy-MM-dd"); //get last modified date for video
+                else
+                    fileDate = GetDateTakenFromImage(paths[i]).ToString("yyyy-MM-dd"); //get date this image was taken
+
+                //add to list if it's not there
+                if (!sortedDict.ContainsKey(fileDate))
+                {
+                    //add this date and create an empty list for the image path(s) that will go in this folder
+                    sortedDict.Add(fileDate, new List<string>());
+                }
+
+                //add this image path!
+                sortedDict[fileDate].Add(paths[i]);
+            }
+
+            return sortedDict;
+        }
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                try
+                {
+                    //sometimes this property isn't there - like when it's not from a camera
+                    PropertyItem propItem = myImage.GetPropertyItem(36867); //Date Taken
+                    string dateTaken = reg.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                    return DateTime.Parse(dateTaken);
+                }
+                catch (Exception)
+                {
+                    return DateTime.MinValue; //return the min value (translates to string 0001-01-01)
+                }
+            }
+        }
+
+        //retrieves the last modiefied date WITHOUT loading the whole image
+        public static DateTime GetDateModified(string path)
+        {
+            try
+            {
+                FileInfo file1 = new FileInfo(path);
+                if (file1.LastWriteTime != null)
+                    return file1.LastWriteTime;
+                else
+                    return DateTime.MinValue; //return the min value (translates to string 0001-01-01)
+            }
+            catch (Exception)
+            {
+                return DateTime.MinValue; //return the min value (translates to string 0001-01-01)
+            }
+        }
+    }
+}
